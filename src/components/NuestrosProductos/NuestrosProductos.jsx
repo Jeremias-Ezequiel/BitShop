14ab0a2531f6ccd1c0d5bf14639b589f530b1d04
Jeremias <jeremias150817@gmail.com>
2025-07-16 18:51:27 -0300

    feat: se agrego manejo de errores y mensajes

--- src/components/NuestrosProductos/NuestrosProductos.jsx
+++ src/components/NuestrosProductos/NuestrosProductos.jsx
@@ -4,11 +4,15 @@ import "./NuestrosProductos.css";
 
 function NuestrosProductos() {
   const [productos, setProductos] = useState([]);
+  const [cargando, setCargando] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://686bb363e559eba90873aee6.mockapi.io/productos")
       .then((response) => response.json())
-      .then((data) => setProductos(data));
+      .then((data) => setProductos(data))
+      .catch((error) => setError("No se pudieron cargar los productos"))
+      .finally(() => setCargando(false));
   }, []);
 
   return (
@@ -16,17 +20,21 @@ function NuestrosProductos() {
       <h2 className="text-center my-3 fw-bold text-uppercase">
         Nuestros Productos
       </h2>
-      <div className="d-flex flex-wrap gap-5 justify-content-center my-5">
-        {productos.map((item) => (
-          <Producto
-            key={item.id}
-            nombre={item.nombre}
-            descripcion={item.descripcion}
-            imagen={item.imagen}
-            precio={item.precio}
-          />
-        ))}
-      </div>
+      {cargando && <p className="text-center m-5 fs-5">Cargando productos..</p>}
+      {error && <p>{error}</p>}
+      {!cargando && !error && (
+        <div className="d-flex flex-wrap gap-5 justify-content-center my-5">
+          {productos.map((item) => (
+            <Producto
+              key={item.id}
+              nombre={item.nombre}
+              descripcion={item.descripcion}
+              imagen={item.imagen}
+              precio={item.precio}
+            />
+          ))}
+        </div>
+      )}
     </>
   );
 }
