6166f09c7b5052a88bdf62a4d2d78a2793f175fc
Jeremias <jeremias150817@gmail.com>
2025-07-18 13:32:56 -0300

    Se arreglo la responsividad en la seccion de categorias

--- src/components/infoBlock/InfoBlock.jsx
+++ src/components/infoBlock/InfoBlock.jsx
@@ -3,23 +3,23 @@ import "./InfoBlock.css";
 function InfoBlock() {
   return (
     <div>
-      <ul className="info-list d-flex m-5 justify-content-around align-items-center text-center">
-        <li className="info-item border-end border-3">
+      <ul className="info-list d-flex m-5 justify-content-around align-items-center text-center row">
+        <li className="info-item  col-12 my-4 col-xl-3">
           <h4 className="my-4">Métodos de pago</h4>
           <p>Aceptamos tarjetas, transferencias y pagos en efectivo</p>
           <img src="./iconos/paid-method.svg" alt="" />
         </li>
-        <li className="info-item border-end border-3">
+        <li className="info-item  col-12 my-4 col-xl-3">
           <h4 className="my-4">Envíos gratis</h4>
           <p>Envíos sin costo en compras superiores a $40.000</p>
-          <img src="./iconos/transporter.svg" alt="" />
+          <img src="./iconos/transporter.svg" alt="" />z
         </li>
-        <li className="info-item border-end border-3">
+        <li className="info-item  col-12 my-4 col-xl-3">
           <h4 className="my-4">Atención al cliente</h4>
           <p>Soporte personalizado 24/7 vía WhatsApp y correo</p>
           <img src="./iconos/support.svg" alt="" />
         </li>
-        <li className="info-item">
+        <li className="info-item col-12 my-4 col-xl-3">
           <h4 className="my-4">Compra 100% segura</h4>
           <p>Pregemos tus datos y transacciones</p>
           <img src="./iconos/secure.svg" alt="" />
